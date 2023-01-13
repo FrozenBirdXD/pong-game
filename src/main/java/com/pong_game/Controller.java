c4beb67fd80295ed6f3034daaecc172107bee425
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-01-13 17:13:31 +0100

    separated gui from the game mechanics to contoller

---
+++ src/main/java/com/pong_game/Controller.java
@@ -0,0 +1,168 @@
+package com.pong_game;
+
+import java.util.Random;
+import java.util.Set;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class Controller {
+    
+    Random rand = new Random();
+    int randomY = rand.nextInt(700) + 50;
+    
+    public int velocityX = 6;
+    public int velocityY = 6;
+    public int ballCenterX = 600;
+    public int ballCenterY = randomY;
+    public int scorePlayer1 = 0;
+    public int scorePlayer2 = 0;
+    public int slider1Y = 330;
+    public int slider2Y = 330;
+    public int sliderSpeed = 10;
+    public boolean paused = false;
+    public boolean aPlayerWon = false;
+    
+    public Circle ball;
+    public Rectangle slider1;
+    public Rectangle slider2;
+    public Text scorePlayer1Text;
+    public Text scorePlayer2Text;
+    public Text winner;
+    public Set keyPressed;
+    public Text instruction;
+    public Text instruction3;
+    
+    public Controller(Circle ball, Rectangle slider1, Rectangle slider2, Text scorePlayer1Text, Text scorePlayer2Text, Text winner, Set keyPressed, Text instruction, Text instruction3) {
+        this.ball = ball;
+        this.slider1 = slider1;
+        this.slider2 = slider2;
+        this.scorePlayer1Text = scorePlayer1Text;
+        this.scorePlayer2Text = scorePlayer2Text;
+        this.winner = winner;
+        this.keyPressed = keyPressed;
+        this.instruction = instruction;
+        this.instruction3 = instruction3;
+    }
+    public void gameTicks() {
+        // main game mechanics
+        AnimationTimer gameTicks = new AnimationTimer() {
+            private long prevTime = 0;
+            
+            @Override
+            public void handle(long now) {
+                long dt = now - prevTime;
+                
+                // checks if the game is not paused and if the time difference from the animation update is bigger that the given value
+                if (!paused && dt > 3e7) {
+                    prevTime = now;
+                    keyboardInput(keyPressed, slider1, slider2, instruction, instruction3);
+                    ballCollision(ball, slider1, slider2);
+                    scorePoint(ball, slider1, slider2, scorePlayer2Text, scorePlayer1Text);
+                    ifPlayerWins(winner);
+                    if (aPlayerWon) {
+                        stop();
+                    }
+                    updateBallPosition(ball);
+                }
+            }
+        };
+        
+        gameTicks.start();
+    }
+    
+    public void ballCollision(Circle ball, Rectangle slider1, Rectangle slider2) {
+        // ball collision detection with slider 1
+        if (ball.getBoundsInParent().intersects(slider1.getBoundsInParent())) {
+            velocityX =- velocityX;
+        }
+        
+        // ball collision detection with slider 2
+        if (ball.getBoundsInParent().intersects(slider2.getBoundsInParent())) {
+            velocityX =- velocityX;
+        }
+        
+        // ball collision detection with boundaries
+        if (ball.getCenterY() >= 745 || ball.getCenterY() <= 20) {
+            velocityY =- velocityY;
+        }
+    }
+    
+    public void updateBallPosition(Circle ball) {
+        ball.setCenterX(ballCenterX += velocityX);
+        ball.setCenterY(ballCenterY += velocityY);
+    }
+    
+    // checks if the ball scored a point and resets the game
+    public void scorePoint(Circle ball, Rectangle slider1, Rectangle slider2, Text scorePlayer2Text, Text scorePlayer1Text) {
+        if (ballCenterX - ball.getRadius() <= 0) {
+            this.scorePlayer2 += 1;
+            String scoreR1 = String.valueOf(this.scorePlayer2);
+            scorePlayer2Text.setText(scoreR1);
+            reset(ball, slider1, slider2);
+        }
+        
+        if (ballCenterX + ball.getRadius() >= 1200) {
+            this.scorePlayer1 += 1;
+            String scoreR1 = String.valueOf(this.scorePlayer1);
+            scorePlayer1Text.setText(scoreR1);
+            reset(ball, slider1, slider2);
+        }
+    }
+    
+    public void reset(Circle ball, Rectangle slider1, Rectangle slider2) {
+        slider1Y = 330;
+        slider2Y = 330;
+        ballCenterX = 600;
+        
+        slider1.setY(slider1Y);
+        slider2.setY(slider2Y);
+        ball.setCenterX(ballCenterX);
+    }
+    
+    public void ifPlayerWins(Text winner) {
+        if (this.scorePlayer1 == 5) {
+            winner.setText("Player 1 WINS!");
+            aPlayerWon = true;
+        }
+        
+        if (this.scorePlayer2 == 5) {
+            winner.setText("Player 2 WINS!");
+            aPlayerWon = true;
+        }
+    }
+    
+    public void keyboardInput(Set keyPressed, Rectangle slider1, Rectangle slider2, Text instruction, Text instruction3) {
+        if (keyPressed.contains(KeyCode.W)) {
+            if (slider1Y - sliderSpeed > -20) {
+                slider1Y -= sliderSpeed;
+            }
+            slider1.setY(slider1Y);
+        } else if (keyPressed.contains(KeyCode.S)) {
+            if (slider1Y + sliderSpeed < 631) {
+                slider1Y += sliderSpeed;
+            }
+            slider1.setY(slider1Y);
+        } else if (keyPressed.contains(KeyCode.NUMPAD8)) {
+            if (slider2Y - sliderSpeed > -20) {
+                slider2Y -= sliderSpeed;
+            }
+            slider2.setY(slider2Y);
+        } else if (keyPressed.contains(KeyCode.NUMPAD5)) {
+            if (slider2Y - sliderSpeed < 631) {
+                slider2Y += sliderSpeed;
+            }
+            slider2.setY(slider2Y);
+        } else if (keyPressed.contains(KeyCode.SPACE)) {
+            instruction.setOpacity(0);
+            instruction3.setOpacity(0);
+        }
+    }
+    
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+}
---
+++ src/main/java/com/pong_game/GUI.java
@@ -0,0 +1,146 @@
+package com.pong_game;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public boolean paused = false;
+
+    public static void main(String[] args) {
+        // launches start method
+        launch(args);  
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+
+        // creates root node and scene 
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root, Color.LIGHTGRAY);
+
+        // adds pongIcon.png as the icon
+        Image icon = new Image("pongIcon.png");
+        stage.getIcons().add(icon);
+
+        // creates a random interger between 50 and 750
+        Random rand = new Random();
+        int randomY = rand.nextInt(700) + 50;
+
+        // creates a ball node
+        // constructor: CenterX, CenterY, Radius, Color
+        Circle ball = new Circle(600, randomY, 20, Color.rgb(212, 130, 47));
+        
+        // creates slider1 node (left)
+        // constructor: LeftCornerX, LeftCornerY, Width, Height
+        Rectangle slider1 = new Rectangle(50, 330, 20, 140);
+        slider1.setArcHeight(5.0);
+        slider1.setArcWidth(5.0);
+        slider1.setFill(Color.rgb(127, 144, 255));
+        slider1.setStroke(Color.BLACK);
+        slider1.setStrokeType(StrokeType.INSIDE);
+        
+        // creates slider2 node (right)
+        // constructor: LeftCornerX, LeftCornerY, Width, Height
+        Rectangle slider2 = new Rectangle(1130, 330, 20, 140);
+        slider2.setArcHeight(5.0);
+        slider2.setArcWidth(5.0);
+        slider2.setFill(Color.rgb(127, 144, 255));
+        slider2.setStroke(Color.BLACK);
+        slider2.setStrokeType(StrokeType.INSIDE);
+
+        // creates winning text
+        Text winner = new Text(480, 486, "");
+        winner.setTextAlignment(TextAlignment.CENTER);  
+        winner.setFont(Font.font("Veranda", 40));
+
+        // creates (temporary) instructions
+        // constructor: OriginX, OriginY, "Text"
+        Text instruction = new Text(384, 486, "use 'w' and 's' for player 1 use 'numpad 8' and 'numpad 5' for player 2");
+        instruction.setFont(Font.font("Veranda", 14));
+
+        Text instruction3 = new Text(482, 505, "Press Spacebar to remove instructions");
+        instruction3.setFont(Font.font("Veranda", 14));
+        
+        // creates scoreboard
+        Text scorePlayer1Text = new Text(485, 98, "0");
+        scorePlayer1Text.setFont(Font.font("Veranda", 60));
+        scorePlayer1Text.setTextAlignment(TextAlignment.CENTER);
+        scorePlayer1Text.setWrappingWidth(158.9366455078125);
+        scorePlayer1Text.setOpacity(0.7);
+
+        Text scorePlayer2Text = new Text(554, 98, "0");
+        scorePlayer2Text.setFont(Font.font("Veranda", 60));
+        scorePlayer2Text.setTextAlignment(TextAlignment.CENTER);
+        scorePlayer2Text.setWrappingWidth(158.9366455078125);
+        scorePlayer2Text.setOpacity(0.7);
+
+        Text colon = new Text(592, 98, ":");
+        colon.setFont(Font.font("Veranda", 60));
+        colon.setOpacity(0.7);
+        
+        // adds all the node to the AnchorPane
+        root.getChildren().addAll(ball, slider1, slider2, scorePlayer1Text, scorePlayer2Text, colon, winner, instruction, instruction3);
+
+        // stage configs
+        stage.setScene(scene);
+        stage.setTitle("Pong");
+        stage.setResizable(false);
+        stage.setWidth(1210);
+        stage.setHeight(800);
+        stage.show();
+
+        // create a set that stores all of the keys that are pressed at any moment
+        Set<KeyCode> keyPressed = new HashSet<KeyCode>();
+
+        // creates controller object
+        Controller controller = new Controller(ball, slider1, slider2, scorePlayer1Text, scorePlayer2Text, winner, keyPressed, instruction, instruction3);
+        
+        // calls the main gameticks method on the controller
+        controller.gameTicks();
+
+        // add an event handler to the scene to listen for keys pressed
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent key) {
+                
+                if (key.getCode().equals(KeyCode.P)) {
+                    paused = !paused;
+                    controller.setPaused(paused);
+                } else {
+                    keyPressed.add(key.getCode());
+                }
+            }
+        });
+
+        // add an event handler to the scene to listen for keys released
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent key) {
+                keyPressed.remove(key.getCode());
+            }
+        });   
+        
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
